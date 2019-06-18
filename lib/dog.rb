955a3d7b4ec8997df8c13651c2aee127c12c2a50
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-18 22:42:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,16 @@ class Dog
     @name = new_name
   end
 
-    def name
-      @name
-    end
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end    
 
 end
